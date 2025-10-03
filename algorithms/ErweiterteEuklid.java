e41ab3ce3a96bfcb120f05f1245b7bc6cbbdbc4c
thatmanhal <81680462+manhal99-hash@users.noreply.github.com>
2025-10-03 13:40:42 +0200

    Add files via upload

---
+++ algorithms/ErweiterteEuklid.java
@@ -0,0 +1,9 @@
+package Krypto2;
+import java.math.BigInteger;
+public class ErweiterteEuklid {
+
+    public static void main(String[]args){
+        BigInteger x = new BigInteger("6884956397045344220809746629001649093037950200943055203735601445031516197751");
+        BigInteger y = new BigInteger("");
+    }
+}
